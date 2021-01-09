eb0c5f58c68451a23ecb99ce86108a846537904d
Dmitrii Shitikov <dshitikov@rtech.ru>
2021-01-09 18:15:58 +0300

    спрайты в автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,7 @@ exports.createWebp = createWebp;
 // Sprite
 
 const sprite = () => {
-  return gulp.src("source/img/icons/*.svg")
+  return gulp.src("source/img/*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
