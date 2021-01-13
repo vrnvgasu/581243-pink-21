a84334a0f93556e7c845e9ce6164f044edf18656
Dmitrii Shitikov <dshitikov@rtech.ru>
2021-01-13 19:36:38 +0300

    переключение инструментов фото

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,12 @@
 'use strict';
 (() => {
   const WORK_PANEL_ACTIVE_CLASS = `main-nav__work-panel--active`;
+  const UPLOAD_ITEM_CLASS = `upload__item`;
   const NAV_TOGGLE_CLOSE_CLASS = `menu-toggle--close`;
   const MAIN_NAV_CLOSE_CLASS = `main-nav__list--close`;
   const POPUP_ACTIVE_CLASS = `popup--active`;
+  const UPLOAD_ITEM_ACTIVE_CLASS = `upload__item--active`;
+  const SCALE_ACTIVE_CLASS = `scale--active`;
 
   let menuToggleElement = document.querySelector(`.menu-toggle`);
   let mainNavWorkPanelElement = document.querySelector(`.main-nav__work-panel`);
@@ -14,6 +17,8 @@
   let popupErrorElement = document.querySelector(`.popup-error`);
   let popupButtonSuccessElement = document.querySelector(`.popup__button.button--success`);
   let mainNavToggleElement = document.querySelector(`.main-nav__toggle`);
+  let uploadItemElements = document.querySelectorAll(`.upload__item`);
+  let scaleElements = document.querySelectorAll(`.scale`);
 
   let closeMenu = () => {
     menuToggleElement.classList.remove(NAV_TOGGLE_CLOSE_CLASS);
@@ -65,9 +70,38 @@
       menuToggleElement.click();
     }
   });
+
   if (formMainElement) {
     submitMainElement.addEventListener(`click`, submitMainElementOnClick);
     popupButtonSuccessElement.addEventListener(`click`, popupButtonSuccessElementOnClick);
+  }
+
+  if (uploadItemElements.length) {
+    document.addEventListener(`click`, (evt) => {
+        let target = evt.target;
+
+        while (target.tagName !== 'BODY') {
+          if (target.classList.contains(UPLOAD_ITEM_CLASS)) {
+            Array.from(uploadItemElements).forEach(item => {
+              item.classList.remove(UPLOAD_ITEM_ACTIVE_CLASS);
+            });
+
+            target.classList.add(UPLOAD_ITEM_ACTIVE_CLASS);
+
+            Array.from(scaleElements).forEach(item => {
+              if (item.parentElement.classList.contains(UPLOAD_ITEM_ACTIVE_CLASS)) {
+                item.classList.add(SCALE_ACTIVE_CLASS);
+              } else {
+                item.classList.remove(SCALE_ACTIVE_CLASS);
+              }
+            });
+
+            break;
+          }
+
+          target = target.parentElement;
+        }
+    });
   }
 
   closeMenu();
